dc893900f808ffb1c811f0ab932df39d6e999664
Mamali <mohammadsafari770@gmail.com>
2026-02-23 17:28:08 +0330

    CRUD for Category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::paginate(2);
+
+        return $this->responser([
+            "categories" => CategoryResource::collection($categories),
+            "links" => CategoryResource::collection($categories)->response()->getData()->links,
+            "meta" => CategoryResource::collection($categories)->response()->getData()->meta
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "parent_id" => "nullable|exists:categories,id",
+            "name" => "required",
+            "description" => "nullable|string"
+        ]);
+
+        if ($validator->fails()) {
+            return $this->responser(null, 422, $validator->messages());
+        }
+
+        $parentId = $request->parent_id === null ? 0 : $request->parent_id;
+
+        $category = Category::create([
+            "parent_id" => $parentId,
+            "name" => $request->name,
+            "description" => $request->description
+        ]);
+
+        return $this->responser(new CategoryResource($category), 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        return $this->responser(new CategoryResource($category), 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Category $category)
+    {
+        $validator = Validator::make($request->all(), [
+            "parent_id" => "nullable|exists:categories,id",
+            "name" => "required",
+            "description" => "nullable|string"
+        ]);
+
+        if ($validator->fails()) {
+            return $this->responser(null, 422, $validator->messages());
+        }
+
+        $parentId = $request->parent_id === null ? 0 : $request->parent_id;
+
+        $category->update([
+            "parent_id" => $parentId,
+            "name" => $request->name,
+            "description" => $request->description
+        ]);
+
+        return $this->responser(new CategoryResource($category), 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return $this->responser(new CategoryResource($category), 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('brands' , BrandController::class);
+
+Route::apiResource('categories' , CategoryController::class);
